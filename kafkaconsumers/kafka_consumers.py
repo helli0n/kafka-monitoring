e3c5a6463142f046430e4408ce5a5fca44e34c9c
Dmytro Vedetskyi <dvedetskyi@dvedetskyi-mbp.local>
2020-08-17 16:47:56 +0300

    Added advanced partition monitoring

--- kafkaconsumers/kafka_consumers.py
+++ kafkaconsumers/kafka_consumers.py
@@ -10,7 +10,8 @@ def my_help():
          ./kafka_consumers.py consumer_status {#CONSUMER}\n\
          ./kafka_consumers.py consumer_tottallag {#CONSUMER}\n\
          ./kafka_consumers.py consumer_offsets {#CONSUMER} start|end {#PARTITION} {#TOPIC}\n\
-         ./kafka_consumers.sh consumer_offsets_status {#CONSUMER} start|end {#PARTITION} {#TOPIC}\n\
+         ./kafka_consumers.py consumer_offsets_partition_status  {#CONSUMER} {#PARTITION} {#TOPIC}\n\
+         ./kafka_consumers.py consumer_offsets_partition_current_lag {#CONSUMER} {#PARTITION} {#TOPIC}\n\
          ./kafka_consumers.py consumer_lag {#CONSUMER} start|end {#PARTITION} {#TOPIC}")
 
 host1 = socket.gethostbyname(socket.gethostname())
@@ -31,7 +32,7 @@ else:
                    #print(r1.json()["status"]["group"])
                    for p in r1.json()["status"]["partitions"]:
                     result["data"].append({
-                       "#{CONSUMER}": consumers,
+                       "{#CONSUMER}": consumers,
                        "{#PARTITION}": p["partition"],
                        "{#TOPIC}": p["topic"]
                     })
@@ -54,14 +55,19 @@ else:
         elif sys.argv[1] == "consumer_lag" and len(sys.argv) == 6:
             r1 = requests.get(url+sys.argv[2]+'/lag')
             for p in r1.json()["status"]["partitions"]:
-                if p["partition"] == int(sys.argv[4]) and p["topic"] == sys.argv[5]:
+                if p["partition"] == int(sys.argv[4]) and p["topic"] == sys.argv[5] and p[sys.argv[3]] != None:
                     lag_status = p[sys.argv[3]]
                     print(lag_status["lag"])
-        elif sys.argv[1] == "consumer_offsets_status" and len(sys.argv) == 6:
+        elif sys.argv[1] == "consumer_offsets_partition_status" and len(sys.argv) == 5:
             r1 = requests.get(url+sys.argv[2]+'/lag')
             for p in r1.json()["status"]["partitions"]:
-                if p["partition"] == int(sys.argv[4]) and p["topic"] == sys.argv[5]:
+                if p["partition"] == int(sys.argv[3]) and p["topic"] == sys.argv[4]:
                     print(Dict.get(p["status"]))
+        elif sys.argv[1] == "consumer_offsets_partition_current_lag" and len(sys.argv) == 5:
+            r1 = requests.get(url+sys.argv[2]+'/lag')
+            for p in r1.json()["status"]["partitions"]:
+                if p["partition"] == int(sys.argv[3]) and p["topic"] == sys.argv[4]:
+                    print((p["current_lag"]))
         else:
             my_help()
     except ValueError:
